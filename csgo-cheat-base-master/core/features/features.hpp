312dafaccd7474d4c68a603d967166222f791b28
techprimedev <techprimedev@hotmail.com>
2020-06-30 20:59:14 -0300

    update base

--- csgo-cheat-base-master/core/features/features.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../../dependencies/common_includes.hpp"
-#include "../novoMenu/variables.hpp"
-
-namespace misc {
-	namespace movement {
-		void bunny_hop(c_usercmd* cmd);
-	};
-}
--- csgo-cheat-base-master/core/features/misc/engine_prediction.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "../../../dependencies/common_includes.hpp"
-
-namespace prediction {
-	void start(c_usercmd* cmd);
-	void end();
-};
